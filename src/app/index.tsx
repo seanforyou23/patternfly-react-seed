6bd3b609f7abec47ff1db3921753e45391ddee4c
Michael Spaxman <seanforyou23@gmail.com>
2019-06-03 11:13:10 -0400

    chore(debug): looking for pf3/pf4 header background width issue
    pull in pf4 page component
    pull in pf3 stylesheet

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
-import { Alert, AlertActionCloseButton, BackgroundImage, BackgroundImageSrc } from '@patternfly/react-core';
+import {
+  Alert,
+  AlertActionCloseButton,
+  BackgroundImage,
+  BackgroundImageSrc,
+  Page,
+  PageHeader,
+  PageSidebar,
+  PageSection,
+  PageSectionVariants
+} from '@patternfly/react-core';
 import '@app/app.css';
+import '@pf3/dist/css/patternfly-react.css';
 import xs from '@assets/images/pfbg_576.jpg';
 import xs2x from '@assets/images/pfbg_576@2x.jpg';
 import sm from '@assets/images/pfbg_768.jpg';
@@ -17,16 +28,50 @@ const images = {
   [BackgroundImageSrc.filter]: `${filter}#image_overlay`
 };
 
-export default class App extends Component {
+export interface AppState {
+  isNavOpen: boolean;
+  isShowing: boolean;
+}
+
+class App extends Component<{}, AppState> {
+  constructor(props) {
+    super(props);
+    this.onNavToggle = this.onNavToggle.bind(this);
+    this.dismissNotification = this.dismissNotification.bind(this);
+  }
   public state = {
-    isShowing: true
+    isShowing: true,
+    isNavOpen: true
   };
+  public onNavToggle() {
+    this.setState({
+      isNavOpen: !this.state.isNavOpen
+    });
+  }
   public render() {
-    const { isShowing } = this.state;
+    const { isShowing, isNavOpen } = this.state;
+    const logoProps = {
+      href: 'https://patternfly.org',
+      target: '_blank'
+    };
+    const Header = (
+      <PageHeader
+        logo="Logo"
+        logoProps={logoProps}
+        toolbar="Toolbar"
+        avatar=" | Avatar"
+        showNavToggle
+        isNavOpen={isNavOpen}
+        onNavToggle={this.onNavToggle}
+      />
+    );
+    const Sidebar = <PageSidebar nav="Navigation" isNavOpen={isNavOpen} />;
+
     return (
       <React.Fragment>
-        <BackgroundImage src={images} />
-        <div className="app-container">
+        {/* <BackgroundImage src={images} /> */}
+        <Page header={Header} sidebar={Sidebar}>
+          <PageSection variant={PageSectionVariants.dark}>
           {isShowing && (
             <div className="notification-container">
               <Alert
@@ -38,7 +83,14 @@ export default class App extends Component {
               </Alert>
             </div>
           )}
-        </div>
+          {!isShowing && (
+            <div>
+              Section with dark background
+            </div>
+          )}
+          </PageSection>
+          <PageSection variant={PageSectionVariants.light}>Stub Patternfly Section</PageSection>
+        </Page>
       </React.Fragment>
     );
   }
@@ -46,3 +98,5 @@ export default class App extends Component {
     this.setState({ isShowing: false });
   };
 }
+
+export { App };
