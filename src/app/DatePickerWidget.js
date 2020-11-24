4440f676db1dd20239f0c91f6ef377b151dbeaa7
Michael Spaxman <seanforyou23@gmail.com>
2020-11-24 17:06:06 -0500

    suggested-updates

--- src/app/DatePickerWidget.js
+++ src/app/DatePickerWidget.js
@@ -3,7 +3,7 @@ import { DatePicker } from '@patternfly/react-datetime';
 
 const DatePickerWidget = () => (
   <>
-    <DatePicker minDate="04/09/1988" maxDate="11/18/2020" />
+    <DatePicker value="2020-03-05" />
   </>
 )
 
