c30b4d3c67b4c777d57de832d18cd632b82836d3
Michael Spaxman <seanforyou23@gmail.com>
2020-12-24 16:11:43 -0500

    add support for styling markdown

--- src/app/Dashboard/Dashboard.tsx
+++ src/app/Dashboard/Dashboard.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { PageSection, Title } from '@patternfly/react-core';
 import markdown from '@app/Dashboard/markdown-test.md';
+import 'github-markdown-css/github-markdown.css';
 
 const Dashboard: React.FunctionComponent = () => (
   <PageSection>
     <Title headingLevel="h1" size="lg">Dashboard Page Title</Title>
-    <div dangerouslySetInnerHTML={{ __html: markdown }} />
+    <div className="markdown-body" dangerouslySetInnerHTML={{ __html: markdown }} />
   </PageSection>
 )
 
--- webpack.prod.js
+++ webpack.prod.js
@@ -33,7 +33,8 @@ module.exports = merge(common('production'), {
           path.resolve(__dirname, 'node_modules/@patternfly/react-core/dist/esm/@patternfly/patternfly'),
           path.resolve(__dirname, 'node_modules/@patternfly/react-core/node_modules/@patternfly/react-styles/css'),
           path.resolve(__dirname, 'node_modules/@patternfly/react-table/node_modules/@patternfly/react-styles/css'),
-          path.resolve(__dirname, 'node_modules/@patternfly/react-inline-edit-extension/node_modules/@patternfly/react-styles/css')
+          path.resolve(__dirname, 'node_modules/@patternfly/react-inline-edit-extension/node_modules/@patternfly/react-styles/css'),
+          path.resolve(__dirname, 'node_modules/github-markdown-css/github-markdown.css')
         ],
         use: [MiniCssExtractPlugin.loader, 'css-loader']
       }
