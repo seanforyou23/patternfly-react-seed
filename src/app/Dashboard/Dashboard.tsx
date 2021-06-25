2a37068311ab53aaab82611ec820c57f657e8839
Michael Spaxman <seanforyou23@gmail.com>
2021-06-25 09:32:35 -0400

    add react-query into example

--- src/app/Dashboard/Dashboard.tsx
+++ src/app/Dashboard/Dashboard.tsx
@@ -17,28 +17,76 @@ import {
 } from '@patternfly/react-core';
 import { useHistory } from 'react-router-dom';
 import { InfoAltIcon, UserAltIcon } from '@patternfly/react-icons'; '@patternfly/react-icons';
+import { useQuery } from 'react-query';
+
+type usersResponse = {
+  id: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+  avatar: string;
+}
+
+type withoutEmail = {
+  id: number;
+  first_name: string;
+  last_name: string;
+  avatar: string;
+}
 
 const Dashboard: React.FunctionComponent = () => {
   const [hasData, setHasData] = React.useState<any>([]);
 
   const history = useHistory();
-  const getContributors = () => {
+
+  const getContributors = (): Promise<usersResponse[]> => {
     return fetch('https://reqres.in/api/users')
+      .then((result) => result.json())
+      .then((data) => data.data)
   }
 
-  React.useEffect(() => {
-    getContributors().then((result) => result.json()).then(data => {
-      setHasData(data.data);
+  const useContributors = () => {
+    return useQuery<usersResponse[], unknown, withoutEmail[]>({
+      queryKey: 'users',
+      queryFn: getContributors,
+      select: (tmpData) => {
+        const foo = tmpData.map(el => {
+          const { email, ...rest } = el;
+          return rest;
+        });
+        return foo;
+      }
     })
-  }, []);
+  }
+
+  const { data, isLoading, error } = useContributors();
+  // console.log(data);
+
+  // React.useEffect(() => {
+  //   // getContributors().then((result) => result.json()).then(data => {
+  //   //   setHasData(data.data);
+  //   // })
+
+  // }, []);
+
+  React.useEffect(() => {
+    // getContributors().then((result) => result.json()).then(data => {
+    //   setHasData(data.data);
+    // })
+    console.log(data);
+
+  }, [data]);
+
+  if (isLoading) return <div>loading</div>
+  if (error) return <div>ERROR</div>
 
   return (
     <PageSection>
       <TextContent>
         <Title headingLevel="h1" className="pf-u-mb-sm">Users</Title>
       </TextContent>
-      {hasData !== [] && <Gallery hasGutter>{
-        (hasData.map((data, idx) => {
+      {data && <Gallery hasGutter>{
+        (data.map((data, idx) => {
           return (
             <GalleryItem key={idx}>
               <Card>
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,23 @@
 import * as React from 'react';
 import '@patternfly/react-core/dist/styles/base.css';
 import '@patternfly/react-styles/css/utilities/Spacing/spacing.css';
+import { QueryClient, QueryCache, QueryClientProvider } from 'react-query';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AppLayout } from '@app/AppLayout/AppLayout';
 import { AppRoutes } from '@app/routes';
 import '@app/app.css';
 
+const queryCache = new QueryCache();
+const queryClient = new QueryClient({ queryCache });
+
 const App: React.FunctionComponent = () => (
-  <Router>
-    <AppLayout>
-      <AppRoutes />
-    </AppLayout>
-  </Router>
+  <QueryClientProvider client={queryClient}>
+    <Router>
+      <AppLayout>
+        <AppRoutes />
+      </AppLayout>
+    </Router>
+  </QueryClientProvider>
 );
 
 export default App;
